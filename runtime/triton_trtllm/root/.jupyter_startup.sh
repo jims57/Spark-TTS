2835b87d4cc341004cdcbaa3dad73cd3c75070c2
jims57 <jims57@163.com>
2025-04-06 11:15:26 +0800

    [ok] change to root folder and add TTS batch processor

--- runtime/triton_trtllm/root/.jupyter_startup.sh
+++ runtime/triton_trtllm/root/.jupyter_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup jupyter notebook --ip=0.0.0.0 --port=9000 --no-browser --allow-root --notebook-dir=~/ > /var/log/jupyter.log 2>&1 &
+nohup jupyter notebook --ip=0.0.0.0 --port=9000 --no-browser --allow-root --notebook-dir=/ > /var/log/jupyter.log 2>&1 &
 echo "Jupyter Notebook started in background on port 9000"
 
 cd ~/Spark-TTS/runtime/triton_trtllm
@@ -9,3 +9,7 @@ echo "Spark-Triton-server HttpService is running in background on port 9002"
 
 nohup python server-api.py > server.log 2>&1 &
 echo "Spark-tts Restful api is running in background on port 9003"
+
+# Run TTS batch processor
+nohup python3 tts_batch_processor.py --total-sentences 20 --total_sentences_per_batch 10 > batch_processor.log 2>&1 &
+echo "TTS batch processor started in background"
